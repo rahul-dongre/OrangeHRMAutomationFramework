13a11410c722ffbc903bbb94d070d73ce65b6763
rahuldongre964@gmail.com <rahuldongre964@gmail.com>
2025-02-01 22:00:38 +0530

    First Project Commit

---
+++ src/main/java/orangeHRMFramework/orangeHRMFramework/App.java
@@ -0,0 +1,13 @@
+package orangeHRMFramework.orangeHRMFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
